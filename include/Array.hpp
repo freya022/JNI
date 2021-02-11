f2ab5a4b99e39d33adbfc43d7d7703bc4bb35a5e
freya022 <41875020+freya022@users.noreply.github.com>
2021-02-11 14:35:38 +0100

    Replace env with GetEnv

--- include/Array.hpp
+++ include/Array.hpp
@@ -4,42 +4,44 @@
 
 #include "jni.h"
 #include <algorithm>
+#include "Helper.hpp"
 
 namespace JNI {
 	template<class T>
 	class Array {
 	private:
-		JNIEnv* env;
 		jboolean copy{};
 	public:
-		void* array{};
+		void *array{};
 		T jArray;
 
-		Array(JNIEnv* env, T t) : env(env), jArray(t) {
+		explicit Array(T t) : jArray(t) {
 			static_assert(!std::is_base_of_v<T, jarray>, "Not a jarray");
 
+			JNIEnv *env = GetEnv();
 			if constexpr (std::is_same_v<T, jintArray>) {
 				array = env->GetIntArrayElements(t, &copy);
-			} if constexpr (std::is_same_v<T, jbyteArray>) {
+			} else if constexpr (std::is_same_v<T, jbyteArray>) {
 				array = env->GetByteArrayElements(t, &copy);
 			}
 		}
 
 		~Array() {
 			if (copy) {
+				JNIEnv* env = GetEnv();
 				if constexpr (std::is_same_v<T, jintArray>) {
-					env->ReleaseIntArrayElements(jArray, (jint*) array, JNI_ABORT);
+					env->ReleaseIntArrayElements(jArray, (jint *) array, JNI_ABORT);
 				} else if constexpr (std::is_same_v<T, jbyteArray>) {
-					env->ReleaseByteArrayElements(jArray, (jbyte*) array, JNI_ABORT);
+					env->ReleaseByteArrayElements(jArray, (jbyte *) array, JNI_ABORT);
 				}
 			}
 		}
 
 		auto arr() {
 			if constexpr (std::is_same_v<T, jintArray>) {
-				return (jint*) array;
+				return (jint *) array;
 			} else if constexpr (std::is_same_v<T, jbyteArray>) {
-				return (jbyte*) array;
+				return (jbyte *) array;
 			}
 		}
 	};
